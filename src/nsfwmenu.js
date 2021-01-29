04a4fe4e918a6c63c6634deddf0307c7a59bda20
nabuto0 <77943167+nabuto0@users.noreply.github.com>
2021-01-29 10:10:40 -0400

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -1,17 +1,17 @@
 const nsfwmenu = (prefix, pushname) => {
     return `Oiin resumindo aqui e o menu NSFW
     ◪ *NSFW*
-  │
-  ├─ ❏ ${prefix}nsfwbobs
-  ├─ ❏ ${prefix}randomhentaio
-  ├─ ❏ ${prefix}nsfwtrap
-  ├─ ❏ ${prefix}nsfwass
-  ├─ ❏ ${prefix}nsfwbelly
-  ├─ ❏ ${prefix}nsfwsidebobs
-  ├─ ❏ ${prefix}nsfwahegao
-  ├─ ❏ ${prefix}nsfwthighs
-  ├─ ❏ ${prefix}nsfwarmpits
-  └─ ❏ ${prefix}nsfwfeets
+  *Em teste*
+  🔰 ${prefix}nsfwbobs
+  🔰 ${prefix}randomhentaio
+  🔰 ${prefix}nsfwtrap
+  🔰 ${prefix}nsfwass
+  🔰 ${prefix}nsfwbelly
+  🔰 ${prefix}nsfwsidebobs
+  🔰 ${prefix}nsfwahegao
+  🔰 ${prefix}nsfwthighs
+  🔰 ${prefix}nsfwarmpits
+  🔰 ${prefix}nsfwfeets
 
   _obs para usar esses comandos ative o menu NSFW_\n _Digite_\n ${prefix}*nsfw 1*`
 
